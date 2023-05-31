ce1b9621c89d42406a050e45ec31d356334121fe
BenZen59 <benoit.zenone@gmail.com>
2023-05-31 11:41:19 +0200

    Add distance beetween two address

--- src/main/scala/example/APICaller.scala
+++ src/main/scala/example/APICaller.scala
@@ -6,6 +6,7 @@ import play.api.libs.json._
 
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
+import scala.util.{Failure, Success}
 
 object APICaller {
   def main(args: Array[String]): Unit = {
@@ -17,6 +18,7 @@ object APICaller {
     val apiKey = "c071e4936018043b17d463c556194f93"
     // Effectuer une requête pour obtenir la géolocalisation d'une adresse
     val address = "1600 Amphitheatre Parkway, Mountain View, CA"
+    val address2 = "Champ de Mars, 5 Av. Anatole France, 75007 Paris"
 
     val requestUri = Uri(s"http://api.positionstack.com/v1/forward")
       .withQuery(
@@ -26,9 +28,18 @@ object APICaller {
         )
       )
 
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = requestUri))
+    val requestUri2 = Uri(s"http://api.positionstack.com/v1/forward")
+      .withQuery(
+        Uri.Query(
+          "access_key" -> apiKey,
+          "query" -> address2
+        )
+      )
 
-    responseFuture.flatMap { response =>
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = requestUri))
+    val responseFuture2: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = requestUri2))
+
+    val futureResult1: Future[Double] = responseFuture.flatMap { response =>
       response.status match {
         case StatusCodes.OK =>
           // Lire la réponse JSON
@@ -44,32 +55,76 @@ object APICaller {
             (latitude, longitude) match {
               case (Some(lat), Some(lon)) =>
                 // Utilisation des valeurs de latitude et de longitude
-                val distance = calculateDistance(lat, lon)
-                println(s"Distance: $distance")
+                Future.successful(calculateDistance(lat, lon))
 
               case _ =>
                 println("Erreur lors de l'extraction des données de latitude et de longitude")
+                Future.failed(new RuntimeException("Erreur lors de l'extraction des données de latitude et de longitude"))
             }
-
-            Future.successful(())
           }
 
         case _ =>
           // Gestion des erreurs de requête
           println(s"Request failed with status code ${response.status}")
           response.discardEntityBytes()
-          Future.successful(())
+          Future.failed(new RuntimeException(s"Request failed with status code ${response.status}"))
       }
-    }.onComplete { _ =>
-      // Fermer le système d'acteurs et le matérialiseur
-      system.terminate()
     }
 
-    def calculateDistance(latitude: Double, longitude: Double): Double = {
-      // Exemple de calcul de distance factice
-      // Vous pouvez implémenter votre propre logique de calcul de distance ici
-      val distance = Math.sqrt(latitude * latitude + longitude * longitude)
-      distance
+    val futureResult2: Future[Double] = responseFuture2.flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          // Lire la réponse JSON
+          response.entity.toStrict(2.seconds).flatMap { entity =>
+            val responseBody = entity.data.utf8String
+
+            // Analyser le contenu JSON
+            val json = Json.parse(responseBody)
+            val latitude = (json \ "data" \\ "latitude").headOption.flatMap(_.asOpt[Double])
+            val longitude = (json \ "data" \\ "longitude").headOption.flatMap(_.asOpt[Double])
+
+            // Vérifier si les valeurs de latitude et de longitude sont présentes
+            (latitude, longitude) match {
+              case (Some(lat), Some(lon)) =>
+                // Utilisation des valeurs de latitude et de longitude
+                Future.successful(calculateDistance(lat, lon))
+
+              case _ =>
+                println("Erreur lors de l'extraction des données de latitude et de longitude")
+                Future.failed(new RuntimeException("Erreur lors de l'extraction des données de latitude et de longitude"))
+            }
+          }
+
+        case _ =>
+          // Gestion des erreurs de requête
+          println(s"Request failed with status code ${response.status}")
+          response.discardEntityBytes()
+          Future.failed(new RuntimeException(s"Request failed with status code ${response.status}"))
+      }
     }
 
-  }}
+    val futureCalculations: Future[Double] = for {
+      result1 <- futureResult1
+      result2 <- futureResult2
+    } yield {
+      result1 + result2
+    }
+
+    futureCalculations.onComplete {
+      case Success(distance) =>
+        println(s"Distance: $distance")
+        system.terminate()
+
+      case Failure(ex) =>
+        println(s"Erreur lors du calcul de la distance : ${ex.getMessage}")
+        system.terminate()
+    }
+  }
+
+  def calculateDistance(latitude: Double, longitude: Double): Double = {
+    // Exemple de calcul de distance factice
+    // Vous pouvez implémenter votre propre logique de calcul de distance ici
+    val distance = Math.sqrt(latitude * latitude + longitude * longitude)
+    distance
+  }
+}
